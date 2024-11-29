75441cebc92ca119cc4071ba8b3d06d230d04d3c
admin <admin@DESKTOP-9U2MBLB>
2024-11-29 21:29:58 +0200

    Add project files.

---
+++ 88.4.1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+abstract class GeographicalObject
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public GeographicalObject(double x, double y, string name, string description)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+    }
+
+    // Віртуальний метод для отримання інформації про об'єкт
+    public virtual string GetInfo()
+    {
+        return $"Name: {Name}\nDescription: {Description}\nCoordinates: ({X}, {Y})";
+    }
+}
+
+
+
+
+class River : GeographicalObject
+{
+    public double FlowSpeed { get; set; } // Швидкість течії у см/с
+    public double Length { get; set; } // Довжина у км
+
+    public River(double x, double y, string name, string description, double flowSpeed, double length)
+        : base(x, y, name, description)
+    {
+        FlowSpeed = flowSpeed;
+        Length = length;
+    }
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $"\nFlow Speed: {FlowSpeed} cm/s\nLength: {Length} km";
+    }
+}
+
+
+
+
+class Mountain : GeographicalObject
+{
+    public double HighestPoint { get; set; } // Найвища точка у метрах
+
+    public Mountain(double x, double y, string name, string description, double highestPoint)
+        : base(x, y, name, description)
+    {
+        HighestPoint = highestPoint;
+    }
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $"\nHighest Point: {HighestPoint} m";
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        River river = new River(48.45, 34.98, "Dnipro", "The longest river in Ukraine", 80, 2201);
+        Console.WriteLine(river.GetInfo());
+        Console.WriteLine();
+
+        Mountain mountain = new Mountain(46.38, 10.82, "Hoverla", "The highest mountain in Ukraine", 2061);
+        Console.WriteLine(mountain.GetInfo());
+    }
+}
+
+
---
+++ 88.4.2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+interface GeographicalObject1
+{
+    double X { get; set; }
+    double Y { get; set; }
+    string Name { get; set; }
+    string Description { get; set; }
+
+    string GetInfo();
+}
+
+
+
+
+class River : GeographicalObject1
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double FlowSpeed { get; set; }
+    public double Length { get; set; }
+
+    public River(double x, double y, string name, string description, double flowSpeed, double length)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+        FlowSpeed = flowSpeed;
+        Length = length;
+    }
+
+    public string GetInfo()
+    {
+        return $"Name: {Name}\nDescription: {Description}\nCoordinates: ({X}, {Y})\nFlow Speed: {FlowSpeed} cm/s\nLength: {Length} km";
+    }
+}
+
+
+
+class Mountain : GeographicalObject1
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double HighestPoint { get; set; }
+
+    public Mountain(double x, double y, string name, string description, double highestPoint)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+        HighestPoint = highestPoint;
+    }
+
+    public string GetInfo()
+    {
+        return $"Name: {Name}\nDescription: {Description}\nCoordinates: ({X}, {Y})\nHighest Point: {HighestPoint} m";
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        GeographicalObject1 river = new River(48.45, 34.98, "Dnipro", "The largest river in Ukraine", 80, 2201);
+        Console.WriteLine(river.GetInfo());
+        Console.WriteLine();
+
+        GeographicalObject1 mountain = new Mountain(46.38, 10.82, "Hoverla", "The highest mountain in Ukraine", 2061);
+        Console.WriteLine(mountain.GetInfo());
+    }
+}
